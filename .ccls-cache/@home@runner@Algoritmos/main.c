dc92ba3d6c512014b6fd02f8ecee73c84040f5d1
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-10-13 11:25:08 +0000

    Tentativa de correção problema estacionamento

--- .ccls-cache/@home@runner@Algoritmos/main.c
+++ .ccls-cache/@home@runner@Algoritmos/main.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 
 int main() {
-  int c, n, tarifa, faturamento, ocupado;
-  int estacionamento[10000];
-  int tamanhos[10000];
-  
+  int c, n, tarifa, faturamento, ocupado, t = 10;
+  int estacionamento[t];
+
   while (scanf("%d %d", &c, &n) != EOF) {
     tarifa = 10;
     faturamento = 0;
     ocupado = 0;
 
-    //scanf("%d %d", &c, &n);
-
-    for (int i = 0; i < c; i++) {
+    for (int i = 0; i < t; i++) {
       estacionamento[i] = 0;
     }
 
     for (int i = 0; i < n; i++) {
       char e;
       int p, q;
+      // e = 'C'; p = 1234; q = 2;
       scanf("%c %d %d", &e, &p, &q); // Lê evento, placa e tamanho do carro
-      if (e == 'C') { // evento de entrada de carro
-        if (ocupado + q <= c) { // verifica se tem espaço livre no estacionamento  para o carro
-          for (int j = 0; j < c; j++) {
-            
-            int temVaga = 1;
-            int vaga = 0;
-            
-            for (int k = vaga; k < q; k++) {
+      if (e == 'C') {                // evento de entrada de carro
+        if (ocupado + q <=
+            c) { // verifica se tem espaço livre no estacionamento  para o carro
+          int temVaga = 1;
+          int vaga = 0;
+          for (int j = 0; j < c;
+               j++) { // verifica espaço por espaço do estacionamento
+            int zero = 0;
+            for (int k = vaga + j; k < q;
+                 k++) { // procura por um espaço livre igual ao carro
+              if (estacionamento[k] == 0) {
+                zero = 0;
+              }
               if (estacionamento[k] > 0) {
                 temVaga = 0;
                 printf("%d", estacionamento[k]);
@@ -35,23 +38,19 @@ int main() {
                 break;
               }
             }
-            
-            if (temVaga == 1) {
+
+            if (temVaga == 1 || zero == 0) {
               for (int k = vaga; k < q; k++) {
                 estacionamento[k] = p;
               }
+              for (int k = 0; k < c; k++) {
+                printf("%d ", estacionamento[k]);
+              }
+              printf("\n");
               ocupado += q;
               faturamento += tarifa;
               break;
             }
-
-            for (int k = 0; k < c; k++) {
-              printf("%d ", estacionamento[k]);
-            }
-            
-            
-            
-            
           }
         }
       } else { // evento de saída de carro
@@ -70,3 +69,6 @@ int main() {
   return 0;
 }
 
+/*
+clang -g -Wno-everything -pthread -lm -O0 ./main.c -o "main-debug"
+  */
