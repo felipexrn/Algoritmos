e1a9cb4940018b26dad39e750557fe5a13f425b4
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-20 21:53:46 +0000

    lista_02_04.c resolvida

--- lista_02/lista_02_04.c
+++ lista_02/lista_02_04.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+void resetar(int repetidos[], int t) {
+  for(int i = 0; i < t; i++) {
+    repetidos[i] = 0;
+  }
+}
+void configurar(int aleatorios[], int t) {
+  srand(time(NULL));
+  for(int i = 0; i < t; i++) {
+    int tempo = rand() % t;
+    aleatorios[i] = tempo + 1;
+  }
+}
+void contar(int aleatorios[], int repetidos[], int t) {
+  for(int i = 1; i <= t; i++) {
+    repetidos[aleatorios[i-1]-1] += 1;
+  }
+}
+void mostrar(int repetidos[], int t) {
+  for(int i = 0; i < t; i++) {
+    if(repetidos[i] > 0) {
+      printf("%d apareceu %d vezes\n", i+1, repetidos[i]);
+    } 
+  }
+}
+void printar(int array[], int t) {
+  for(int i = 0; i < t; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
 int main() {
-  int t = 1000000, lista[t]; 
-  
+  int t = 10, repetidos[t], aleatorios[t]; 
+  resetar(repetidos, t);
+  configurar(aleatorios, t);
+  contar(aleatorios, repetidos, t);
+  mostrar(repetidos, t);
+  printar(repetidos, t);
+  printar(aleatorios, t);
   return 0;
 }
