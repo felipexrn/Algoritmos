bffd1ec8c77007cb5c28f11eee7511ace42efedc
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-12 10:14:35 +0000

    correção das questão lista_02_05.c

--- lista_02/lista_02_05.c
+++ lista_02/lista_02_05.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 void zerar(long lista[], long t) {
   for (long i = 0; i < t; i++) {
     lista[i] = 0;
@@ -22,30 +22,23 @@ void imprimir(long lista[], long t) {
   }
   printf("\n");
 }
-void maior_soma(long lista[], long t, long x, long somas[]) { // créditos para Igor por este algorítmo
+void maior_soma(long lista[], long t, long x,
+                long somas[]) { // créditos para Igor por este algorítmo
   long maior = 0;
-  for (long i = 0; i < t; i++) { 
-    for (long j = 0 + i; j < t; j++) {    
-      for (long k = 0 + j; k < t; k++) {
-        for (long l = 0 + k; l < t; l++) {
-          for (long m = 0 + l; m < t; m++) {
+  for (long i = 0; i < t; i++) {
+    for (long j = 1 + i; j < t; j++) {
+      for (long k = 1 + j; k < t; k++) {
+        for (long l = 1 + k; l < t; l++) {
+          for (long m = 1 + l; m < t; m++) {
             long soma = lista[i] + lista[j] + lista[k] + lista[l] + lista[m];
-            if (i != j && i != k && i != l && i != m) {
-              if (j != k && j != l && j != m) {
-                if (k != l && k != m) {
-                  if (l != m) {
-                    if (soma <= x) {
-                      maior = soma;
-                      somas[0] = lista[i];
-                      somas[1] = lista[j];
-                      somas[2] = lista[k];
-                      somas[3] = lista[l];
-                      somas[4] = lista[m];
-                    }
-                  }
-                }
-              }
-            }  
+            if (soma <= x) {
+              maior = soma;
+              somas[0] = lista[i];
+              somas[1] = lista[j];
+              somas[2] = lista[k];
+              somas[3] = lista[l];
+              somas[4] = lista[m];
+            }
           }
         }
       }
@@ -54,19 +47,20 @@ void maior_soma(long lista[], long t, long x, long somas[]) { // créditos para 
 }
 int main() {
   /*
-  long t = 11, x = 850, somas[5], numeros[11] = {2321, 321, 24, 291, 19, 301, 502, 10020, 30201, 2001, 10};
+  long t = 11, x = 850, somas[5], numeros[11] = {2321, 321, 24, 291, 19, 301,
+  502, 10020, 30201, 2001, 10};
   */
-  
-  long t = 37, x = 1500, numeros[t], somas[5]; 
+
+  long t = 37, x = 1500, numeros[t], somas[5];
   zerar(numeros, t);
   numerar(numeros, t);
-  //imprimir(numeros, t);
-  clock_t inicio,fim;
+  // imprimir(numeros, t);
+  clock_t inicio, fim;
   inicio = clock();
   maior_soma(numeros, t, x, somas);
   fim = clock();
-  double tempo = ((double)(fim-inicio)) / (CLOCKS_PER_SEC/1000.0);
-  printf("(%.4lf ms) ",tempo);
+  double tempo = ((double)(fim - inicio)) / (CLOCKS_PER_SEC / 1000.0);
+  printf("(%.4lf ms) ", tempo);
   imprimir(somas, 5);
   return 0;
 }
