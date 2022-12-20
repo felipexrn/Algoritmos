195a4b9e24f6bad6a3779502f5e360ee2533e91c
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-20 22:03:33 +0000

    array struct

--- .ccls-cache/@home@runner@Algoritmos/struct@conceito.c
+++ .ccls-cache/@home@runner@Algoritmos/struct@conceito.c
@@ -14,7 +14,30 @@ int main() {
   p1.idade = 30;
   printf("%s\n", p1.nome);
   printf("%.2f\n", p1.altura);
-  printf("%d\n", p1.idade);
+  printf("%d\n\n", p1.idade);
+
+  struct pessoa amigos[3];
+  char n0[] = "Gabriel";
+  strcpy(amigos[0].nome, n0);
+  amigos[0].altura = 1.83;
+  amigos[0].idade = 35;
+
+  char n1[] = "Cinara";
+  strcpy(amigos[1].nome, n1);
+  amigos[1].altura = 1.77;
+  amigos[1].idade = 33;
+
+  char n2[] = "Guaxa";
+  strcpy(amigos[2].nome, n2);
+  amigos[2].altura = 1.9;
+  amigos[2].idade = 45;
+
+  for (int i = 0; i < 3; i++) {
+    printf("%s\n", amigos[i].nome);
+    printf("%.2f\n", amigos[i].altura);
+    printf("%d\n\n", amigos[i].idade);
+  }
+  
   return 0;
 }
  
