1f19873901921debf0f9ab5e8ae085139d65de6a
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-02-06 13:31:07 +0000

    l04_03 erro

--- forca_bruta/l04_03.c
+++ forca_bruta/l04_03.c
@@ -20,18 +20,23 @@ void print_maze(int lin, int col, int maze[7][7]) {
   }
 }
 
-int find_exit_way(int lin, int col, int maze[7][7]) {
+int find_exit_way(int l, int c, int lin, int col, int maze[7][7]) {
   int ans = 0;
-  if ()
+  if (maze[l][c] != 0 || l == lin || c == col || l < 0 || c < 0) return 0;
+  if (l == lin-1 && c == col-1 ) return 1;
+  ans = ans + find_exit_way(l+1, c, lin, col, maze);
+  ans = ans + find_exit_way(l, c+1, lin, col, maze);
+  ans = ans + find_exit_way(l-1, c, lin, col, maze);
+  ans = ans + find_exit_way(l, c-1, lin, col, maze);
   return ans;
 }
 
 int main() {
-  int lin, col, maze[7][7];
+  int l = 0, c = 0, lin, col, maze[7][7];
   scanf("%d %d", &lin, &col);
   read_maze(lin, col, maze);
   //print_maze(lin, col, maze);
-  int ans = find_exit_way(lin, col, maze);
+  int ans = find_exit_way(l, c, lin, col, maze);
   printf("%d\n", ans);
   return 0;
 }
