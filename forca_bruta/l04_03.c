5be311c95b3e8313d022ea784e5a6b0bd130521a
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-02-07 01:32:31 +0000

    l04_03.c em desenvolvimento

--- forca_bruta/l04_03.c
+++ forca_bruta/l04_03.c
@@ -20,23 +20,32 @@ void print_maze(int lin, int col, int maze[7][7]) {
   }
 }
 
-int find_exit_way(int l, int c, int lin, int col, int maze[7][7]) {
-  int ans = 0;
+int find_exit_ways(int l, int c, int lin, int col, int maze[7][7]) {
+  if (l == lin-1 && c == col-1 ) {
+    printf("\n");
+    print_maze(lin, col, maze);
+    return 1;
+  }
   if (maze[l][c] != 0 || l == lin || c == col || l < 0 || c < 0) return 0;
-  if (l == lin-1 && c == col-1 ) return 1;
-  ans = ans + find_exit_way(l+1, c, lin, col, maze);
-  ans = ans + find_exit_way(l, c+1, lin, col, maze);
-  ans = ans + find_exit_way(l-1, c, lin, col, maze);
-  ans = ans + find_exit_way(l, c-1, lin, col, maze);
+  int ans = 0; 
+  maze[l][c] = 2;
+  ans += find_exit_ways(l+1, c, lin, col, maze);
+  ans += find_exit_ways(l, c+1, lin, col, maze);
+  ans += find_exit_ways(l-1, c, lin, col, maze);
+  ans += find_exit_ways(l, c-1, lin, col, maze);
   return ans;
+}
+
+int find_exit_ways_recursive(int l, int c, int lin, int col, int maze[7][7]) {
+  return find_exit_ways(l, c, lin, col, maze);
 }
 
 int main() {
   int l = 0, c = 0, lin, col, maze[7][7];
   scanf("%d %d", &lin, &col);
   read_maze(lin, col, maze);
-  //print_maze(lin, col, maze);
-  int ans = find_exit_way(l, c, lin, col, maze);
+  print_maze(lin, col, maze);
+  int ans = find_exit_ways(l, c, lin, col, maze);
   printf("%d\n", ans);
   return 0;
 }
---
+++ forca_bruta/maze.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+struct caminho{
+  int tamanho;
+  int l[110],c[110];
+};
+
+void print_caminho(struct caminho *cam) {
+  printf("Caminho encontrado: ");
+  for (int i=0 ; i<cam->tamanho ; ++i)
+    printf("(%d,%d) ",cam->l[i],cam->c[i]);
+  printf("\n");
+}
+
+int find_lab_bt(int lab[10][10], int lin, int col, int l, int c, struct caminho *cam){
+  if (l==lin-1 && c==col-1) {
+    print_caminho(cam);
+  return 1;
+  }
+  
+  if (lab[l][c]!=0 || l<0 || c<0 || l==lin || c==col){
+    return 1000000;
+  }
+  
+  int ans = 1000000;
+  lab[l][c]=2;
+  cam->l[cam->tamanho] = l;
+  cam->c[cam->tamanho] = c;
+  cam->tamanho++;
+  ans = find_lab_bt(lab,lin,col,l+1,c, cam);
+  int ans_alt = find_lab_bt(lab,lin,col,l,c+1,cam);
+  if (ans_alt < ans) ans = ans_alt;
+    ans_alt = find_lab_bt(lab,lin,col,l-1,c,cam);
+  if (ans_alt < ans) ans = ans_alt;
+    ans_alt = find_lab_bt(lab,lin,col,l,c-1,cam);
+  if (ans_alt < ans) ans = ans_alt;
+    lab[l][c]=0;
+  cam->tamanho--;
+  return ans+1;
+}
+
+int find_lab(int lab[10][10], int lin, int col, struct caminho *cam){
+  return find_lab_bt(lab,lin,col,0,0, cam);
+}
+
+void read_lab(int lab[10][10], int lin, int col){
+  int i,j;
+  for (i=0 ; i<lin ; ++i)
+  for (j=0 ; j<col ; ++j)
+  scanf("%d",&lab[i][j]);
+}
+
+int main() {
+  int lab[10][10],lin,col;
+  struct caminho cam;
+  cam.tamanho=0;
+  scanf("%d %d",&lin,&col);
+  read_lab(lab,lin,col);
+  int sol = find_lab(lab,lin,col,&cam);
+  printf("%d\n",sol);
+  return 0;
+}
